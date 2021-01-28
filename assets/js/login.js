dc00e5b4d6390b3df47ba798046069740370aa87
welove <kidding@qq.com>
2021-01-28 16:45:31 +0800

    login over

---
+++ assets/js/login.js
@@ -0,0 +1,68 @@
+$(function () {
+    $('#link-reg').on('click', function () {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+    $('#link-login').on('click', function () {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+    let form = layui.form
+    form.verify({
+        // 自定义了一个叫做 pwd 校验规则
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        // 校验两次密码是否一致的规则
+        repwd: function (value) {
+            // 通过形参拿到的是确认密码框中的内容
+            // 还需要拿到密码框中的内容
+            // 然后进行一次等于的判断
+            // 如果判断失败,则return一个提示消息即可
+            // 后代选择器+属性选择器
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致！'
+            }
+        }
+    })
+    let layer = layui.layer
+    $('#form_reg').on('submit', function (e) {
+        e.preventDefault()
+        let data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }
+        // let data = $(this).serialize()
+        $.ajax({
+            type: 'post',
+            url: '/api/reguser',
+            data,
+            success(res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message);
+                }
+                layer.msg(res.message);
+                $('#link-login').click()
+            }
+        })
+    })
+    $('#form_login').submit(function (e) {
+        // 阻止默认提交行为
+        e.preventDefault()
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            // 快速获取表单中的数据
+            data: $(this).serialize(),
+            success(res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败！')
+                }
+                layer.msg('登录成功！')
+                // 将登录成功得到的 token 字符串，保存到 localStorage 中
+                localStorage.setItem('token', res.token)
+                // 跳转到后台主页
+                location.href = '/home/index.html'
+            }
+        })
+    })
+})
