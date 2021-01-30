f9eed63cc3eaa54779302e01af5b6b0011c595a0
welove <kidding@qq.com>
2021-01-30 15:33:46 +0800

    index.html over

--- assets/js/baseAjax.js
+++ assets/js/baseAjax.js
@@ -1,3 +1,22 @@
 $.ajaxPrefilter(function (option) {
     option.url = "http://ajax.frontend.itheima.net" + option.url
+    if (option.url.indexOf('/my/') !== -1) {
+        option.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    option.complete = function (res) {
+        // console.log('执行了 complete 回调：')
+        // console.log(res)
+        // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1. 强制清空 token
+            layui.layer.alert('身份认证失败！请重新登录', function (index) {
+                localStorage.removeItem('token')
+                // 2. 强制跳转到登录页面
+                location.href = '/login.html'
+                layui.layer.close(index)
+            })
+        }
+    }
 })
--- assets/js/login.js
+++ assets/js/login.js
@@ -61,7 +61,7 @@ $(function () {
                 // 将登录成功得到的 token 字符串，保存到 localStorage 中
                 localStorage.setItem('token', res.token)
                 // 跳转到后台主页
-                location.href = '/home/index.html'
+                location.href = '/index.html'
             }
         })
     })
